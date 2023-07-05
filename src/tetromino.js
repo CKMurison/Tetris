ca5a5f8b50cf3c5d2c35602ce4ccd8a5ee736ee0
Cameron Murison <cmuris4@gmail.com>
2023-07-05 13:25:35 +0100

    updated moveVertical method to include an if/else statement that deals with the player

--- src/tetromino.js
+++ src/tetromino.js
@@ -1,9 +1,14 @@
 class Tetromino {
   moveVertical(player, tetromino) {
     tetromino.position.forEach((blockPosition) => {
-      blockPosition[0] += 1;
+      if (player === 'player1') {
+        blockPosition[0] += 1;
+      } else {
+        blockPosition[0] -= 1;
+      };
     });
   }
 }
 
 module.exports = Tetromino;
+
--- tests/tetromino.test.js
+++ tests/tetromino.test.js
@@ -1,12 +1,22 @@
 const Tetromino = require("../src/tetromino");
 
 describe("tetromino", () => {
-  test("updates the position of the tetromino correctly", () => {
+  test("Updates the position of the tetromino for Player 1", () => {
     const tetromino = new Tetromino();
     tetromino.position = [[0, 0], [0, 1], [0, 2], [0, 3]]
-
     tetromino.moveVertical('player1', tetromino);
 
     expect(tetromino.position).toEqual([[1, 0], [1, 1], [1, 2], [1, 3]]);
+    console.log(tetromino.position)
+
   });
+  test("Updates the position of player 2's tetromino", () => {
+    const tetromino = new Tetromino();
+    tetromino.position = [[1, 0], [1, 1], [1, 2], [1, 3]];
+    tetromino.moveVertical('player2', tetromino);
+
+    expect(tetromino.position).toEqual([[0, 0], [0, 1], [0, 2], [0, 3]]);
+    console.log(tetromino.position)
+  });
+
 });
