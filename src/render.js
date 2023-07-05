36f957e939af16bef7d7d60b088a1163fa76efa9
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-07-05 11:17:10 +0100

    unique identifier for individual cells added

--- src/render.js
+++ src/render.js
@@ -7,13 +7,14 @@ class Render {
     let gridContainer = document.createElement('div');
     gridContainer.className = 'gridContainer';
     
-    grid.forEach((row) => {
+    grid.forEach((row, rowNum) => {
       let rowContainer = document.createElement('div');
       rowContainer.className = 'rowContainer';
 
-      row.forEach((cell) => {
+      row.forEach((cell, colNum) => {
         let cellContainer = document.createElement('div');
         cellContainer.className = cell === 0 ? 'empty' : 'iBlock';
+        cellContainer.id = `${rowNum}.${colNum}`;
 
         rowContainer.append(cellContainer);
       })
