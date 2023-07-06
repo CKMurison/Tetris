5cf2bf949c1fa68c46647f856d8bbf7a117764a4
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-07-06 11:33:31 +0100

    added tests for z-block and both players

--- tests/game.test.js
+++ tests/game.test.js
@@ -103,4 +103,20 @@ describe("tetromino", () => {
     game.moveVertical();
     expect(game.activeTetromino.position).toEqual([[0, 1], [1, 0], [1, 1], [1, 2]])
   });
-});
+  
+  test("Updates the position of the z-block for player 1", () => {
+  mockTetromino = { position:[[1, 0], [1, 1], [2, 1], [2, 2]] }
+  game.activeTetromino = mockTetromino
+  game.activePlayer = 'player1'
+  game.moveVertical();
+  expect(game.activeTetromino.position).toEqual([[2, 0], [2, 1], [3, 1], [3, 2]])
+  });
+ 
+  test("Updates the position of the z-block for player 2", () => {
+  mockTetromino = { position:[[1, 0], [1, 1], [2, 1], [2, 2]] }
+  game.activeTetromino = mockTetromino
+  game.activePlayer = 'player2'
+  game.moveVertical();
+  expect(game.activeTetromino.position).toEqual([[0, 0], [0, 1], [1, 1], [1, 2]])
+  });
+}); 
