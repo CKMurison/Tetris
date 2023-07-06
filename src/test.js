e9410552e22bb00fd3bdd2f75cfce15a30624946
Cameron <cameronjamesbishop@googlemail.com>
2023-07-06 19:51:58 +0100

    mapping out rotation algorithm

--- src/test.js
+++ src/test.js
@@ -8,3 +8,28 @@ for (let i = 0; i < iBlock.length; i++) {
 // grid[0].splice(3,4, iBlock)
 
 console.log(grid)
+
+// rotation research (tf = transformation aka new spot)
+
+const relation = [[-1,1],[-1,0],[0,0],[1,0]]
+
+let newJay = null;
+
+relation.forEach(rel => {
+  let diff = rel[0] + rel[1]
+  if (Math.abs(diff) % 2 === 0) { // for distance from anchor of sqrt(2)
+    switch (diff) {
+      case -2:
+        // tf to 0,+2
+        break
+      case 0: 
+        (Math.sign(rel[0]) === 1 ? /* tf to -2,0 */ -2 : /* tf to +2,0 */ 2 )
+        break
+      case 2:
+        // tf to 0,-2
+        break
+    }
+  } else { // for distance from anchor of 1
+    
+  }
+})
