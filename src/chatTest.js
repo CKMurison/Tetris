79bf9df8da1ec4806476ce67bfe3651e92ffddbc
Cameron <cameronjamesbishop@googlemail.com>
2023-07-06 15:15:46 +0100

    delete gameTest.js

--- src/chatTest.js
+++
@@ -1,8 +0,0 @@
-const grid = Array(20).fill(0).map(() => Array(10).fill(0));
-const iBlock = [1, 1, 1, 1];
-
-for (let i = 0; i < iBlock.length; i++) {
-  grid[10][i + 3] = iBlock[i];
-}
-
-console.log(grid);
