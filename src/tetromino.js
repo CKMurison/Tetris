87d5014adf471de659b2e082f373a38789e46d7f
CyrylG <cyrylgotkowicz@gmail.com>
2023-07-06 11:36:15 +0100

    #checlCollisionDown method implemented and tested for collisions with walls and other blocks

---
+++ src/tetromino.js
@@ -0,0 +1,19 @@
+class Tetromino {
+    constructor() {
+        this.positions = null;
+    }
+
+    checkCollisionDown(grid) {
+        let collision = false;
+        this.positions.forEach((position) => {
+            if (position[0] >= grid.length - 1) {
+                collision = true;
+            } else if (grid[position[0] + 1][position[1]] !== 0) {
+                collision = true;
+            }
+        })
+        return collision;
+    }
+}
+
+module.exports = Tetromino;
---
+++ test/tetromino.test.js
@@ -0,0 +1,35 @@
+const Tetromino = require('../src/tetromino');
+
+describe('Tetromino', () => {
+    it('checks for collision with wall for a single block moving down', () => {
+        const tetromino = new Tetromino();
+        tetromino.positions = [[0, 0]];
+
+        expect(tetromino.checkCollisionDown([[1, 0]])).toBe(true);
+        expect(tetromino.checkCollisionDown([[1, 0],[0, 0]])).toBe(false);
+    });
+
+    it('checks for collision with another piece for a single block moving down', () => {
+        const tetromino = new Tetromino();
+        tetromino.positions = [[0, 0]];
+
+        expect(tetromino.checkCollisionDown([[1, 0], [2, 0]])).toBe(true);
+        expect(tetromino.checkCollisionDown([[1, 0],[0, 0]])).toBe(false);
+    })
+
+    it('checks for collision with wall for a single block moving right', () => {
+        const tetromino = new Tetromino();
+        tetromino.positions = [[0, 0]];
+
+        expect(tetromino.checkCollisionDown([[1, 0], [2, 0]])).toBe(true);
+        expect(tetromino.checkCollisionDown([[1, 0],[0, 0]])).toBe(false);
+    })
+
+    it('checks for collision with wall for a single block moving left', () => {
+        const tetromino = new Tetromino();
+        tetromino.positions = [[0, 0]];
+
+        expect(tetromino.checkCollisionDown([[1, 0], [2, 0]])).toBe(true);
+        expect(tetromino.checkCollisionDown([[1, 0],[0, 0]])).toBe(false);
+    })
+});
