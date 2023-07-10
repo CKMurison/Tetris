fa0e48b257bf7b00a18086bba4a429033dae9d72
CyrylG <cyrylgotkowicz@gmail.com>
2023-07-10 11:32:50 +0100

    Added tests for all left and right keys

--- test/player.test.js
+++ test/player.test.js
@@ -99,4 +99,193 @@ describe('Player', () => {
             ]
         )
     })
+
+    test('press right p1', () => {
+        const mockPressRight = { key: "ArrowRight"}
+        document.addEventListener = jest.fn((event, callback) => {
+            if (event === "keydown") {
+                callback(mockPressRight)
+            }
+        })
+        const inputField = document.createElement('input');
+        document.body.appendChild(inputField);
+        inputField.focus();
+
+        expect(mockPressRight.key).toBe("ArrowRight");
+
+        document.body.removeChild(inputField);
+        let game;
+        game = new Game();
+        game.generateTetromino(2)
+        player = new Player(1, game);
+
+        player.controls()
+        console.log(game.grid);
+        expect(game.grid).toEqual(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
+                [0, 0, 0, 0, 3, 3, 3, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+        )
+    })
+
+    test('press left p2', () => {
+        const mockPressLeft = { key: "a"}
+        document.addEventListener = jest.fn((event, callback) => {
+            if (event === "keydown") {
+                callback(mockPressLeft)
+            }
+        })
+        const inputField = document.createElement('input');
+        document.body.appendChild(inputField);
+        inputField.focus();
+
+        expect(mockPressLeft.key).toBe("a");
+
+        document.body.removeChild(inputField);
+        let game;
+        game = new Game();
+        game.generateTetromino(3)
+        player = new Player(2, game);
+
+        player.controls()
+        console.log(game.grid);
+        expect(game.grid).toEqual(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 4, 4, 0, 0, 0, 0, 0],
+                [0, 0, 0, 4, 4, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+        )
+    })
+
+    test('press right p2', () => {
+        const mockPressRight = { key: "d"}
+        document.addEventListener = jest.fn((event, callback) => {
+            if (event === "keydown") {
+                callback(mockPressRight)
+            }
+        })
+        const inputField = document.createElement('input');
+        document.body.appendChild(inputField);
+        inputField.focus();
+
+        expect(mockPressRight.key).toBe("d");
+
+        document.body.removeChild(inputField);
+        let game;
+        game = new Game();
+        game.generateTetromino(4)
+        player = new Player(2, game);
+
+        player.controls()
+        console.log(game.grid);
+        expect(game.grid).toEqual(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 5, 5, 0, 0, 0],
+                [0, 0, 0, 0, 5, 5, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+        )
+    })
+
+    test('press left p1 twice', () => {
+        const mockPressLeft = { key: "ArrowLeft"}
+        document.addEventListener = jest.fn((event, callback) => {
+            if (event === "keydown") {
+                callback(mockPressLeft)
+            }
+        })
+        const inputField = document.createElement('input');
+        document.body.appendChild(inputField);
+        inputField.focus();
+
+        expect(mockPressLeft.key).toBe("ArrowLeft");
+
+        document.body.removeChild(inputField);
+        let game;
+        game = new Game();
+        game.generateTetromino(0)
+        player = new Player(1, game);
+
+        player.controls()
+        player.controls()
+        console.log(game.grid);
+        expect(game.grid).toEqual(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+        )
+    })
 })
