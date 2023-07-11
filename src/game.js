a3c2e333f698481249146cb2779df21168cdbac3
Cameron <cameronjamesbishop@googlemail.com>
2023-07-11 16:04:33 +0100

    remove commented out old rotation method

--- src/game.js
+++ src/game.js
@@ -187,50 +187,6 @@ class Game {
     this.render.drawGrid(this.grid);
   }
 
-  // rotateTetromino() {
-  //   let anchorPoint = this.activeTetromino.positions[1]
-  //   let relation = [] // [0,-1] [0,0] [1,0] [1,1]
-  //   let afterTF = []
-  //   let newArr = []
-
-  //   this.clearTetromino();
-  //   this.activeTetromino.positions.forEach(arr => {
-  //     relation.push([arr[0] - anchorPoint[0], arr[1] - anchorPoint[1]])
-  //   })
-
-  //   const transformation = {
-  //     "[-1,0]": [0, 1],
-  //     "[0,1]": [1, 0],
-  //     "[1,0]": [0, -1],
-  //     "[0,-1]": [-1, 0],
-  //     "[-1,-1]": [-1, 1],
-  //     "[-1,1]": [1, 1],
-  //     "[1,1]": [1, -1],
-  //     "[1,-1]": [-1, -1],
-  //     "[-2,0]": [0, 2],
-  //     "[0,2]": [2, 0],
-  //     "[2,0]": [0, -2],
-  //     "[0,-2]": [-2, 0],
-  //     "[0,0]" : [0, 0]
-  //   }
-
-  //   relation.forEach(arr => {
-  //     console.log(newArr)
-  //     newArr = transformation[JSON.stringify(arr)]
-  //     console.log(newArr)
-  //     console.log(newArr[0]);
-  //     console.log(newArr[1]);
-
-  //     afterTF.push([newArr[0] + anchorPoint[0],newArr[1] + anchorPoint[1]])
-  //   })
-
-  //   this.activeTetromino.positions = afterTF;
-  //   this.drawTetromino();
-  //   this.render.drawGrid(this.grid);
-  //   console.log(this.activeTetromino.positions);
-  //   return this.activeTetromino.positions;
-  // }
-
   clearTetromino() {
     this.activeTetromino.positions.forEach((eachCoordinate) => {
       this.grid[eachCoordinate[0]][eachCoordinate[1]] = 0
