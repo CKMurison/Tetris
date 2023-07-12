406c1ed550b79d274d1aa802ce05908afd22feec
Aubrey Salmins <aubreysalmins@gmail.com>
2023-07-12 16:08:21 +0100

    Add further changes to restartButton

--- src/render.js
+++ src/render.js
@@ -1,3 +1,5 @@
+const Game = require('./game');
+
 class Render {
   constructor() {
     this.mainEl = document.querySelector('#main-container');
@@ -12,12 +14,12 @@ class Render {
     gridContainer.className = 'gridContainer';
     gridContainer.style.height = "95vh"
     gridContainer.style.width = `calc(95vh / ${grid.length} * ${grid[0].length}`
-    
+
     grid.forEach((row, rowNum) => {
       let rowContainer = document.createElement('div');
       rowContainer.className = 'rowContainer';
       rowContainer.id = `row${rowNum}`;
-      
+
       row.forEach((cell, colNum) => {
         let cellContainer = document.createElement('div');
         cellContainer.className = "cellContainer";
@@ -36,11 +38,11 @@ class Render {
 
   findSpawnLine(grid) {
     let spawnRow = document.querySelector(`#row${grid.length / 2 - 1}`);
-    if (spawnRow !== null) spawnRow.className += " spawnRow"; 
+    if (spawnRow !== null) spawnRow.className += " spawnRow";
   }
 
   #findClassName(cell) {
-    switch(cell){
+    switch (cell) {
       case 0:
         return 'empty';
       case 1:
@@ -65,7 +67,7 @@ class Render {
   pauseText() {
     let pauseContainer = document.createElement('div')
     pauseContainer.className = 'pause'
-    pauseContainer.textContent = 'paused'; 
+    pauseContainer.textContent = 'paused';
     this.mainEl.append(pauseContainer);
     document.querySelectorAll('.cellContainer').forEach((el) => {
       el.style.animationName = "cellAnimation";
@@ -74,29 +76,33 @@ class Render {
 
   restartButton() {
     let restartContainer = document.createElement('div');
+    let button = document.createElement('button');
+    button.id = 'restart_button'
     restartContainer.className = 'restart';
     restartContainer.textContent = '↻';
-  
+    restartContainer.append(button);
+
     // Add event listener for click event
-    restartContainer.addEventListener('click', () => {
+    button.addEventListener('click', () => {
       // Code to execute when the button is clicked
       // For example, you can call a function to handle the restart logic
-      game.restartButton();
+      this.game.restartGame();
+      console.log('button clicked')
     });
-  
-    this.mainEl.append(restartContainer);
+
+    this.mainEl.appendChild(restartContainer);
 
   }
 
   removePauseText() {
     document.querySelector('.pause').remove();
-    
+
   };
 
   gameOver(player) {
     let gameOverContainer = document.createElement('div')
     gameOverContainer.className = 'gameOver'
-    gameOverContainer.textContent = player === 'Player1' ? 'Player 1 Wins!' : 'Player 2 Wins!'; 
+    gameOverContainer.textContent = player === 'Player1' ? 'Player 1 Wins!' : 'Player 2 Wins!';
     this.mainEl.append(gameOverContainer);
     document.querySelectorAll('.cellContainer').forEach((el) => {
       el.style.animationName = "cellAnimation";
